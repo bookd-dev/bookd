6c25f560d7e6a60ed89843dda4797f1ab200fb75
shenchao <a13405665741@gmail.com>
2026-01-23 15:52:25 +0800

    fix static files error

--- src/main/kotlin/com/bookd/plugins/Routing.kt
+++ src/main/kotlin/com/bookd/plugins/Routing.kt
@@ -14,7 +14,9 @@ fun Application.configureRouting() {
     routing {
         // Static files for covers (Docker: /app/covers, Local: ./covers)
         staticFiles("/covers", File("covers"))
-        
+        // Static files for book images (Docker: /app/book_images, Local: ./book_images)
+        staticFiles("/book_images", File("book_images"))
+
         healthRoutes()
         authRoutes(userService)
         userManagementRoutes(userService)
